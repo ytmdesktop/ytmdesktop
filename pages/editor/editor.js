6eedf8d0191f673a590be06e928f88868476f499
Adler Luiz <adlerluiz10@gmail.com>
2019-12-04 09:53:23 -0300

    :art: CSS Editor

--- pages/editor/editor.js
+++ pages/editor/editor.js
@@ -1,10 +1,14 @@
 const { ipcRenderer } = require("electron");
 const path = require("path");
 const fs = require("fs");
+const __ = require("../../providers/translateProvider");
 const themePath = path.join(__dirname, "../../assets/custom-theme.css");
 
 const textEditor = document.getElementById("editor");
 const btnSave = document.getElementById("save");
+const btnClose = document.getElementById("btn-close");
+
+__.loadi18n();
 
 if (fs.existsSync(themePath)) {
   textEditor.innerHTML = fs.readFileSync(themePath).toString();
@@ -14,5 +18,11 @@ if (btnSave) {
   btnSave.addEventListener("click", function() {
     fs.writeFileSync(themePath, textEditor.value);
     ipcRenderer.send("update-custom-theme");
+  });
+}
+
+if (btnClose) {
+  btnClose.addEventListener("click", function() {
+    window.close();
   });
 }
