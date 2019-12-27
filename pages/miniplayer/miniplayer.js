9eae5d2c408e4d39bfa75da8cc1503bae6271768
Rick Palmeira <rickinbsb@gmail.com>
2019-12-27 18:40:20 -0300

    :construction: miniplayer buttons spacing

--- pages/miniplayer/miniplayer.js
+++ pages/miniplayer/miniplayer.js
@@ -1,11 +1,37 @@
 const { ipcRenderer } = require("electron");
 
 const coverbg = document.getElementById("cover-bg");
-const buttonClose = document.getElementById("btn-close");
 
-buttonClose.addEventListener("click", () => {
+const btnClose = document.getElementById("btn-close");
+const btnDislike = document.getElementById("btn-dislike");
+const btnPrevious = document.getElementById("btn-previous");
+const btnPlayPause = document.getElementById("btn-play-pause");
+const btnNext = document.getElementById("btn-next");
+const btnLike = document.getElementById("btn-like");
+
+btnClose.addEventListener("click", () => {
   ipcRenderer.send("restore-main-window");
   window.close();
+});
+
+btnDislike.addEventListener("click", () => {
+  ipcRenderer.send("media-down-vote", true);
+});
+
+btnPrevious.addEventListener("click", () => {
+  ipcRenderer.send("media-previous-track", true);
+});
+
+btnPlayPause.addEventListener("click", () => {
+  ipcRenderer.send("media-play-pause", true);
+});
+
+btnNext.addEventListener("click", () => {
+  ipcRenderer.send("media-next-track", true);
+});
+
+btnLike.addEventListener("click", () => {
+  ipcRenderer.send("media-up-vote", true);
 });
 
 init();
@@ -13,10 +39,10 @@ init();
 function init() {
   setInterval(() => {
     ipcRenderer.send("what-is-song-playing-now");
-  }, 500);
+  }, 1000);
 
   ipcRenderer.on("song-playing-now-is", (e, data) => {
-    console.log(data);
+    // console.log(data);
     setBackgroundCover(data.track.cover);
   });
 }
