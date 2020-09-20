e9a89523b0d7f5d4159eb24384696bfafd645897
Chris Yates <chris.m.yates@outlook.com>
2020-09-19 21:20:35 -0400

    :bug: #398

--- src/utils/injectControls.js
+++ src/utils/injectControls.js
@@ -548,17 +548,20 @@ function createBottomPlayerBarContent() {
 
         setInterval(() => {
             const popup = document.querySelector('.ytmusic-menu-popup-renderer')
-            const addLibrary = Array.from(popup.children).filter(
-                (value) =>
-                    value
-                        .querySelector('g path:not([fill])')
-                        .getAttribute('d') ==
-                        'M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7.53 12L9 10.5l1.4-1.41 2.07 2.08L17.6 6 19 7.41 12.47 14zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6z' ||
-                    value
-                        .querySelector('g path:not([fill])')
-                        .getAttribute('d') ==
-                        'M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z'
-            )[0]
+            let addLibrary
+            if (popup != undefined) {
+                addLibrary = Array.from(popup.children).filter(
+                    (value) =>
+                        value
+                            .querySelector('g path:not([fill])')
+                            .getAttribute('d') ==
+                            'M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7.53 12L9 10.5l1.4-1.41 2.07 2.08L17.6 6 19 7.41 12.47 14zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6z' ||
+                        value
+                            .querySelector('g path:not([fill])')
+                            .getAttribute('d') ==
+                            'M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z'
+                )[0]
+            }
 
             if (addLibrary != undefined && showAddToLibrary) {
                 const _d = addLibrary
