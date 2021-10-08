0a025ffe67486ee1ce6d4f1cb5bc128da42f501e
NovusTheory <novustheory@gmail.com>
2021-10-08 15:47:11 -0500

    Use xosms for windowsMediaProvider

--- src/providers/windowsMediaProvider.js
+++ src/providers/windowsMediaProvider.js
@@ -1,13 +1,4 @@
-const {
-    MediaPlaybackStatus,
-    MediaPlaybackType,
-    SystemMediaTransportControlsButton,
-} = require('@nodert-win10-rs4/windows.media')
-const { BackgroundMediaPlayer } = require('windows.media.playback')
-const {
-    RandomAccessStreamReference,
-} = require('@nodert-win10-rs4/windows.storage.streams')
-const { Uri } = require('@nodert-win10-rs4/windows.foundation')
+const xosms = require('xosms')
 
 const mediaControl = require('../providers/mediaProvider')
 const settingsProvider = require('./settingsProvider')
@@ -16,51 +7,45 @@ class windowsMediaProvider {
     constructor() {
         this._view = null
         this._isInitialized = false
-        this._controls =
-            BackgroundMediaPlayer.current.systemMediaTransportControls
+        this._controls = new xosms.MediaServiceProvider()
 
         if (!settingsProvider.get('settings-windows10-media-service-show-info'))
             this._controls.isEnabled = false
+        else this._controls.isEnabled = true
 
-        this._controls.isChannelDownEnabled = false
-        this._controls.isChannelUpEnabled = false
-        this._controls.isFastForwardEnabled = false
-        this._controls.isRecordEnabled = false
-        this._controls.isRewindEnabled = false
-        this._controls.isNextEnabled = true
-        this._controls.isPauseEnabled = true
-        this._controls.isPlayEnabled = true
-        this._controls.isPreviousEnabled = true
+        this._controls.nextButtonEnabled = true
+        this._controls.pauseButtonEnabled = true
+        this._controls.playButtonEnabled = true
+        this._controls.previousButtonEnabled = true
 
-        this._controls.playbackStatus = MediaPlaybackStatus.closed
-        this._controls.displayUpdater.type = MediaPlaybackType.music
+        this._controls.playbackStatus = xosms.PlaybackStatus.Closed
+        this._controls.mediaType = xosms.MediaType.Music
 
-        this._controls.displayUpdater.musicProperties.title = 'YouTube Music'
-        this._controls.displayUpdater.musicProperties.artist = ''
-        this._controls.displayUpdater.thumbnail = RandomAccessStreamReference.createFromUri(
-            new Uri('https://avatars1.githubusercontent.com/u/48072485?s=500')
+        this._controls.title = 'YouTube Music'
+        this._controls.artist = ''
+        this._controls.SetThumbnail(
+            xosms.ThumbnailType.Uri,
+            'https://avatars1.githubusercontent.com/u/48072485?s=500'
         )
 
-        this._controls.displayUpdater.update()
-
-        this._controls.on('buttonpressed', (sender, eventArgs) => {
-            switch (eventArgs.button) {
-                case SystemMediaTransportControlsButton.play:
+        this._controls.buttonPressed = (button) => {
+            switch (button) {
+                case 'play':
                     mediaControl.playPauseTrack(this._view)
                     break
-                case SystemMediaTransportControlsButton.pause:
+                case 'pause':
                     mediaControl.playPauseTrack(this._view)
                     break
-                case SystemMediaTransportControlsButton.next:
+                case 'next':
                     mediaControl.nextTrack(this._view)
                     break
-                case SystemMediaTransportControlsButton.previous:
+                case 'previous':
                     mediaControl.previousTrack(this._view)
                     break
                 default:
                     break
             }
-        })
+        }
     }
 
     init(view) {
@@ -71,22 +56,17 @@ class windowsMediaProvider {
     setPlaybackStatus(status) {
         if (this._isInitialized) {
             if (status)
-                this._controls.playbackStatus = MediaPlaybackStatus.paused
-            else this._controls.playbackStatus = MediaPlaybackStatus.playing
-
-            this._controls.displayUpdater.update()
+                this._controls.playbackStatus = xosms.PlaybackStatus.Paused
+            else this._controls.playbackStatus = xosms.PlaybackStatus.Playing
         }
     }
 
     setPlaybackData(title, author, cover, album) {
         if (this._isInitialized) {
-            this._controls.displayUpdater.musicProperties.title = title
-            this._controls.displayUpdater.musicProperties.artist = author
-            this._controls.displayUpdater.thumbnail = RandomAccessStreamReference.createFromUri(
-                new Uri(cover)
-            )
-            this._controls.displayUpdater.musicProperties.albumTitle = album
-            this._controls.displayUpdater.update()
+            this._controls.title = title
+            this._controls.artist = author
+            this._controls.SetThumbnail(xosms.ThumbnailType.Uri, cover)
+            this._controls.albumTitle = album
         }
     }
 }
