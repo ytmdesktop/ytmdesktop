ea1b97d1af26dcf2908cd777085a49263240f4a0
IMISSED <Damienweifen@gmail.com>
2022-01-10 14:30:05 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2238,18 +2238,6 @@ powerMonitor.on('suspend', () => {
     }
 })
 
-if (!settingsProvider.get('settings-disable-analytics')) {
-    const analytics = require('./src/providers/analyticsProvider')
-    analytics.setEvent(
-        'main',
-        'start',
-        'v' + app.getVersion(),
-        app.getVersion()
-    )
-    analytics.setEvent('main', 'os', process.platform, process.platform)
-    analytics.setScreen('main')
-}
-
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 const mediaControl = require('./src/providers/mediaProvider')
