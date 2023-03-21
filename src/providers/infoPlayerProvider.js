712896cbdcc8da88b2c99b2ed1390ad3c00f72a4
IMISSED <Damienweifen@gmail.com>
2023-03-20 19:47:59 -0700

    Playlist fixed for api

--- src/providers/infoPlayerProvider.js
+++ src/providers/infoPlayerProvider.js
@@ -392,7 +392,7 @@ function getPlaylist(webContents) {
                 return new Promise( (resolve, reject) => {
                     var popup = document.querySelector('.ytmusic-menu-popup-renderer');
                     var addPlaylist = Array.from(popup.children)
-                        .filter( (value) => value.querySelector('g path:not([fill])').getAttribute('d') == "M14 10H2v2h12v-2zm0-4H2v2h12V6zm4 8v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM2 16h8v-2H2v2z" )[0].querySelector('a')
+                        .filter( (value) => value.querySelector('g path:not([fill])').getAttribute('d') == "M22,13h-4v4h-2v-4h-4v-2h4V7h2v4h4V13z M14,7H2v1h12V7z M2,12h8v-1H2V12z M2,16h8v-1H2V16z")[0].querySelector('a')
                     addPlaylist.click()
                     addPlaylist.click()
 
