af5b04b21a3640359d80af93afd907db9c430f54
Jason Hull <jahull@gmail.com>
2023-08-02 23:39:04 -0700

    Fix the "Add to Playlist" bottom bar button

--- src/utils/injectControls.js
+++ src/utils/injectControls.js
@@ -575,7 +575,7 @@ function createBottomPlayerBarContent() {
                         value
                             .querySelector('g path:not([fill])')
                             .getAttribute('d') ===
-                        'M14 10H2v2h12v-2zm0-4H2v2h12V6zm4 8v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM2 16h8v-2H2v2z'
+                        'M22,13h-4v4h-2v-4h-4v-2h4V7h2v4h4V13z M14,7H2v1h12V7z M2,12h8v-1H2V12z M2,16h8v-1H2V16z'
                 )[0]
                 .querySelector('a')
             addPlaylist.click()
