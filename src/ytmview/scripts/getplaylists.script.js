857b8f9cf78454c8c0fa2f351aaf4c84ad2a4291
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2023-08-24 19:36:53 -0500

    🎨 Run format on split script files

--- src/ytmview/scripts/getplaylists.script.js
+++ src/ytmview/scripts/getplaylists.script.js
@@ -1,29 +1,32 @@
 {
   new Promise((resolve, reject) => {
-    var returnValue = []
+    var returnValue = [];
     var serviceRequestEvent = {
-        bubbles: true,
-        cancelable: false,
-        composed: true,
-        detail: {
-            actionName: 'yt-service-request',
-            args: [
-                document.querySelector("ytmusic-player-bar"),
-                {
-                    addToPlaylistEndpoint: {
-                        videoId: document.querySelector("ytmusic-player-bar").playerApi_.getPlayerResponse().videoDetails.videoId
-                    }
-                }
-            ],
-            optionalAction: false,
-            returnValue
-        }
+      bubbles: true,
+      cancelable: false,
+      composed: true,
+      detail: {
+        actionName: "yt-service-request",
+        args: [
+          document.querySelector("ytmusic-player-bar"),
+          {
+            addToPlaylistEndpoint: {
+              videoId: document.querySelector("ytmusic-player-bar").playerApi_.getPlayerResponse().videoDetails.videoId
+            }
+          }
+        ],
+        optionalAction: false,
+        returnValue
+      }
     };
-    document.querySelector("ytmusic-player-bar").dispatchEvent(new CustomEvent('yt-action', serviceRequestEvent));
-    returnValue[0].ajaxPromise.then((response) => {
-      resolve(response.data.contents[0].addToPlaylistRenderer.playlists);
-    }, () => {
-      reject();
-    });
+    document.querySelector("ytmusic-player-bar").dispatchEvent(new CustomEvent("yt-action", serviceRequestEvent));
+    returnValue[0].ajaxPromise.then(
+      response => {
+        resolve(response.data.contents[0].addToPlaylistRenderer.playlists);
+      },
+      () => {
+        reject();
+      }
+    );
   });
 }
