15f0942eaa2f17cc1f1e35cbbacf813041727596
Alipoodle <ali.poodle12@gmail.com>
2023-09-11 21:04:32 +0100

    🚸 Provide better error for Downloaded builds errors

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -7,8 +7,27 @@ import { plugins } from "./webpack.plugins";
 
 import ChildProcess from 'child_process';
 
-const gitBranch = ChildProcess.execSync("git rev-parse --abbrev-ref HEAD").toString();
-const gitCommitHash = ChildProcess.execSync("git rev-parse HEAD").toString();
+let gitBranch: string = '';
+let gitCommitHash: string = '';
+try {
+  gitBranch = ChildProcess.execSync("git rev-parse --abbrev-ref HEAD").toString();
+  gitCommitHash = ChildProcess.execSync("git rev-parse HEAD").toString();
+}
+catch (e) {
+  // User has likely downloaded from the YTM Desktop via the "Download ZIP".
+  // We don't plan to support this, but at least provide users with a bit of improved UX
+  // by providing them with what to do rather than just leaving them in the dust.
+
+  e.message =
+    " ======= Failed to get Git Info. ======= \n"+
+    "Please make sure that when building this application you are cloning the repository from GitHub rather than using the Download ZIP option.\n"+
+    "Follow the instructions in the README.md file to clone the repository and build the application from there.\n"+
+    " ======= Failed to get Git Info. ======= \n\n" +
+    e.message;
+
+  // Re-throw the error so that the build fails with the updated message.
+  throw e;
+}
 
 rules.push({
   test: /\.css$/,
