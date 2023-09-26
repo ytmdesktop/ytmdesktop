d9a39f16e92d6bf9adb32bdcdf821970736fabb6
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2023-09-25 22:55:09 -0500

    🐛 Fix allow list with all supported google domains

--- src/index.ts
+++ src/index.ts
@@ -809,6 +809,14 @@ const createOrShowSettingsWindow = (): void => {
   settingsWindow.loadURL(SETTINGS_WINDOW_WEBPACK_ENTRY);
 };
 
+function urlIsGoogleAccountsDomain(url: URL): boolean {
+  // https://www.google.com/supported_domains
+  const supportedDomains = [".google.com",".google.ad",".google.ae",".google.com.af",".google.com.ag",".google.al",".google.am",".google.co.ao",".google.com.ar",".google.as",".google.at",".google.com.au",".google.az",".google.ba",".google.com.bd",".google.be",".google.bf",".google.bg",".google.com.bh",".google.bi",".google.bj",".google.com.bn",".google.com.bo",".google.com.br",".google.bs",".google.bt",".google.co.bw",".google.by",".google.com.bz",".google.ca",".google.cd",".google.cf",".google.cg",".google.ch",".google.ci",".google.co.ck",".google.cl",".google.cm",".google.cn",".google.com.co",".google.co.cr",".google.com.cu",".google.cv",".google.com.cy",".google.cz",".google.de",".google.dj",".google.dk",".google.dm",".google.com.do",".google.dz",".google.com.ec",".google.ee",".google.com.eg",".google.es",".google.com.et",".google.fi",".google.com.fj",".google.fm",".google.fr",".google.ga",".google.ge",".google.gg",".google.com.gh",".google.com.gi",".google.gl",".google.gm",".google.gr",".google.com.gt",".google.gy",".google.com.hk",".google.hn",".google.hr",".google.ht",".google.hu",".google.co.id",".google.ie",".google.co.il",".google.im",".google.co.in",".google.iq",".google.is",".google.it",".google.je",".google.com.jm",".google.jo",".google.co.jp",".google.co.ke",".google.com.kh",".google.ki",".google.kg",".google.co.kr",".google.com.kw",".google.kz",".google.la",".google.com.lb",".google.li",".google.lk",".google.co.ls",".google.lt",".google.lu",".google.lv",".google.com.ly",".google.co.ma",".google.md",".google.me",".google.mg",".google.mk",".google.ml",".google.com.mm",".google.mn",".google.com.mt",".google.mu",".google.mv",".google.mw",".google.com.mx",".google.com.my",".google.co.mz",".google.com.na",".google.com.ng",".google.com.ni",".google.ne",".google.nl",".google.no",".google.com.np",".google.nr",".google.nu",".google.co.nz",".google.com.om",".google.com.pa",".google.com.pe",".google.com.pg",".google.com.ph",".google.com.pk",".google.pl",".google.pn",".google.com.pr",".google.ps",".google.pt",".google.com.py",".google.com.qa",".google.ro",".google.ru",".google.rw",".google.com.sa",".google.com.sb",".google.sc",".google.se",".google.com.sg",".google.sh",".google.si",".google.sk",".google.com.sl",".google.sn",".google.so",".google.sm",".google.sr",".google.st",".google.com.sv",".google.td",".google.tg",".google.co.th",".google.com.tj",".google.tl",".google.tm",".google.tn",".google.to",".google.com.tr",".google.tt",".google.com.tw",".google.co.tz",".google.com.ua",".google.co.ug",".google.co.uk",".google.com.uy",".google.co.uz",".google.com.vc",".google.co.ve",".google.co.vi",".google.com.vn",".google.vu",".google.ws",".google.rs",".google.co.za",".google.co.zm",".google.co.zw",".google.cat"]
+  const domain = url.hostname.split("accounts")[1];
+  if (supportedDomains.includes(domain)) return true;
+  return false;
+}
+
 const createYTMView = (): void => {
   memoryStore.set("ytmViewLoadTimedout", false);
 
@@ -845,15 +853,17 @@ const createYTMView = (): void => {
 
   // Attach events to ytm view
   ytmView.webContents.on("will-navigate", event => {
+    const url = new URL(event.url);
+    console.log(url.hostname, url.pathname);
     if (
-      !event.url.startsWith("https://consent.youtube.com/") &&
-      !event.url.startsWith("https://accounts.google.com/") &&
-      !event.url.startsWith("https://accounts.youtube.com/") &&
-      !event.url.startsWith("https://music.youtube.com/") &&
-      !event.url.startsWith("https://www.youtube.com/signin") &&
-      !event.url.startsWith("https://youtube.com/signin") &&
-      !event.url.startsWith("https://www.youtube.com/premium") &&
-      !event.url.startsWith("https://youtube.com/premium")
+      url.hostname !== "consent.youtube.com" &&
+      url.hostname !== "accounts.youtube.com" &&
+      url.hostname !== "music.youtube.com" &&
+      !(url.hostname === "www.youtube.com" && url.pathname === "/signin") &&
+      !(url.hostname === "youtube.com" && url.pathname === "/signin") &&
+      !(url.hostname === "www.youtube.com" && url.pathname === "/premium") &&
+      !(url.hostname === "youtube.com" && url.pathname === "/premium") &&
+      !urlIsGoogleAccountsDomain(url)
     ) {
       event.preventDefault();
       log.info(`Blocking YTM View navigation to ${event.url}`);
@@ -862,15 +872,16 @@ const createYTMView = (): void => {
     }
   });
   ytmView.webContents.on("will-redirect", (event) => {
+    const url = new URL(event.url);
     if (
-      !event.url.startsWith("https://consent.youtube.com/") &&
-      !event.url.startsWith("https://accounts.google.com/") &&
-      !event.url.startsWith("https://accounts.youtube.com/") &&
-      !event.url.startsWith("https://music.youtube.com/") &&
-      !event.url.startsWith("https://www.youtube.com/signin") &&
-      !event.url.startsWith("https://youtube.com/signin") &&
-      !event.url.startsWith("https://www.youtube.com/premium") &&
-      !event.url.startsWith("https://youtube.com/premium")
+      url.hostname !== "consent.youtube.com" &&
+      url.hostname !== "accounts.youtube.com" &&
+      url.hostname !== "music.youtube.com" &&
+      !(url.hostname === "www.youtube.com" && url.pathname === "/signin") &&
+      !(url.hostname === "youtube.com" && url.pathname === "/signin") &&
+      !(url.hostname === "www.youtube.com" && url.pathname === "/premium") &&
+      !(url.hostname === "youtube.com" && url.pathname === "/premium") &&
+      !urlIsGoogleAccountsDomain(url)
     ) {
       event.preventDefault();
       log.info(`Blocking YTM View redirect to ${event.url}`);
