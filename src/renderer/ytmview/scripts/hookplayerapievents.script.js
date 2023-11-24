ea59e0dc7b7e5765f83098477db82998abab379d
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2023-11-24 15:34:42 -0600

    🐛 💩 Fix race condition with player state, fixes #1203

--- src/renderer/ytmview/scripts/hookplayerapievents.script.js
+++ src/renderer/ytmview/scripts/hookplayerapievents.script.js
@@ -1,19 +1,5 @@
 (function() {
-  document.querySelector("ytmusic-player-bar").playerApi.addEventListener("onVideoProgress", progress => {
-    window.ytmd.sendVideoProgress(progress);
-  });
-  document.querySelector("ytmusic-player-bar").playerApi.addEventListener("onStateChange", state => {
-    window.ytmd.sendVideoState(state);
-  });
-  document.querySelector("ytmusic-player-bar").playerApi.addEventListener("onVideoDataChange", event => {
-    if (event.playertype === 1 && (event.type === "dataloaded" || event.type === "dataupdated")) {
-      window.ytmd.sendVideoData(
-        document.querySelector("ytmusic-player-bar").playerApi.getPlayerResponse().videoDetails,
-        document.querySelector("ytmusic-player-bar").playerApi.getPlaylistId()
-      );
-    }
-  });
-  document.querySelector("ytmusic-player-bar").store.subscribe(() => {
+  function sendStoreState() {
     // We don't want to see everything in the store as there can be some sensitive data so we only send what's necessary to operate
     let state = document.querySelector("ytmusic-player-bar").store.getState();
 
@@ -46,6 +32,26 @@
     const adPlaying = state.player.adPlaying;
 
     window.ytmd.sendStoreUpdate(state.queue, album, likeStatus, volume, adPlaying);
+  }
+
+  document.querySelector("ytmusic-player-bar").playerApi.addEventListener("onVideoProgress", progress => {
+    window.ytmd.sendVideoProgress(progress);
+  });
+  document.querySelector("ytmusic-player-bar").playerApi.addEventListener("onStateChange", state => {
+    window.ytmd.sendVideoState(state);
+  });
+  document.querySelector("ytmusic-player-bar").playerApi.addEventListener("onVideoDataChange", event => {
+    if (event.playertype === 1 && (event.type === "dataloaded" || event.type === "dataupdated")) {
+      window.ytmd.sendVideoData(
+        document.querySelector("ytmusic-player-bar").playerApi.getPlayerResponse().videoDetails,
+        document.querySelector("ytmusic-player-bar").playerApi.getPlaylistId()
+      );
+      // This event will reset the video details object in YTMD so we send a store update to ensure a race condition between the store dispatch events and this event aren't occurring
+      sendStoreState();
+    }
+  });
+  document.querySelector("ytmusic-player-bar").store.subscribe(() => {
+    sendStoreState();
   });
   window.addEventListener("yt-action", e => {
     if (e.detail.actionName === "yt-service-request") {
