a37975d037b955021b52797e182d639d40dbf024
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2024-04-24 17:53:27 -0500

    🧑‍💻 Fix webpack native relocator
    Notably `undefinedelectron-log-preload.js` would exist and this corrects that issue

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -4,6 +4,10 @@ import path from "path";
 
 import { rules } from "./webpack.rules";
 
+// This needs an eslint disable for now to clear the warning
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const relocateLoader = require("@vercel/webpack-asset-relocator-loader");
+
 rules.push({
   test: /\.tsx?$/,
   exclude: /(node_modules|\.webpack)/,
@@ -31,7 +35,18 @@ export const mainConfig: Configuration = {
       YTMD_DISABLE_UPDATES: false,
       YTMD_UPDATE_FEED_OWNER: `'${process.env.YTMD_UPDATE_FEED_OWNER}'` ?? "'ytmdesktop'",
       YTMD_UPDATE_FEED_REPOSITORY: `'${process.env.YTMD_UPDATE_FEED_REPOSITORY}'` ?? "'ytmdesktop'"
-    })
+    }),
+    {
+      // This fixes issues with webpack where natives or other were not being properly configured and relocated because of an undefined variable in webpack
+
+      // Waiving this explicit any for now
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      apply(compiler: any) {
+        compiler.hooks.compilation.tap("webpack-asset-relocator-loader", (compilation: unknown) => {
+          relocateLoader.initAssetCache(compilation, "native_modules");
+        });
+      }
+    }
   ],
   resolve: {
     alias: {
