8e74954b22e75e5fcc7cdd10568e6f2e8f8b492d
Alipoodle <ali.poodle12@gmail.com>
2024-04-25 22:03:48 +0100

    ➖🐛[Remote] Remove Fastify/Static, Fix Slider & Errors

--- src/main/integrations/companion-server/remote/index.ts
+++ src/main/integrations/companion-server/remote/index.ts
@@ -1,6 +1,6 @@
+import fs from "fs";
 import path from "path";
 import { FastifyPluginCallback, FastifyPluginOptions } from "fastify";
-import { fastifyStatic } from "@fastify/static";
 import { app } from "electron";
 
 const assetFolder = path.join(
@@ -8,10 +8,28 @@ const assetFolder = path.join(
 );
 const RemoteServer: FastifyPluginCallback<FastifyPluginOptions> = async (fastify, options, next) => {
   // Give the contents of `src` as the root of the server
+  fastify.get("/", async (req, reply) => {
+    const stream = fs.createReadStream(path.join(assetFolder, "index.html"), "utf-8");
+    return reply.type("text/html").send(stream);
+  });
 
-  fastify.register(fastifyStatic, {
-    root: path.join(assetFolder),
-    prefix: "/"
+  fastify.get("/css/control.css", async (req, reply) => {
+    const stream = fs.createReadStream(path.join(assetFolder, "css/control.css"), "utf-8");
+    return reply.type("text/css").send(stream);
+  });
+
+  fastify.get("/js/control.js", async (req, reply) => {
+    const stream = fs.createReadStream(path.join(assetFolder, "js/control.js"), "utf-8");
+    return reply.type("text/javascript").send(stream);
+  });
+
+  fastify.get("/js/socket.io.min.js", async (req, reply) => {
+    const stream = fs.createReadStream(path.join(assetFolder, "js/socket.io.min.js"), "utf-8");
+    return reply.type("text/javascript").send(stream);
+  });
+
+  fastify.get("*", async (req, reply) => {
+    return reply.redirect("/remote");
   });
 
   next();
