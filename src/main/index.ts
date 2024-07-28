51debc29e8bb06539d9b27e45c64507fe473b6bb
Akiisqt <71682867+Akiisqt@users.noreply.github.com>
2024-07-28 05:54:37 -0500

    remove custom error handler

--- src/main/index.ts
+++ src/main/index.ts
@@ -65,18 +65,15 @@ log.transports.file.format = "[{y}-{m}-{d} {h}:{i}:{s}.{ms}][{processType}][{lev
 log.eventLogger.format = "Electron event {eventSource}#{eventName} observed";
 
 log.hooks.push((message, transport) => {
-  // If the transport is not a file transport then return as is
-  if (transport !== log.transports.file) { return message; }
-  // If there isnt message data, or the data isnt a string, or the data is spam from Youtube Music, return false
-  if (message?.data?.[0] && (typeof message.data[0] === 'string' && message.data[0].includes("Third-party cookie will be blocked."))) return false;
+  if (transport !== log.transports.file) return message;
 
-  // Check it is an array, then redact sensitive info
-  message.data = message.data.map(data => {
-    if (typeof data === 'string') {
-      return data.replaceAll(/(?<=((https|http):\/\/)?.{1,64}(\..{1,64})?\..{1,64}\/)([\S]+)/gm, "[REDACTED]");
-    }
-    return data;
-  });
+  // Annoyingly spammed log from YTM that doesn't need to get added to the log file
+  if (message.data[0].includes("Third-party cookie will be blocked.")) return false;
+  for (let i = 0; i < message.data.length; i++) {
+    let dataString: string = message.data[i] as string;
+    dataString = dataString.replaceAll(/(?<=((https|http):\/\/)?.{1,64}(\..{1,64})?\..{1,64}\/)([\S]+)/gm, "[REDACTED]");
+    message.data[i] = dataString;
+  }
 
   return message;
 });
