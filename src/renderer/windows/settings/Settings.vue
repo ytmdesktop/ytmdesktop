9f8ed6902c0922b42aef11debeb4decb1c302274
Akiisqt <71682867+Akiisqt@users.noreply.github.com>
2024-07-28 06:41:54 -0500

    Update Settings.vue
    Refactored
    Making the discord listening type setting disappear if discord presence isnt enabled

--- src/renderer/windows/settings/Settings.vue
+++ src/renderer/windows/settings/Settings.vue
@@ -394,18 +394,16 @@ window.ytmd.handleUpdateDownloaded(() => {
               <td>No authorized companions</td>
             </div>
           </YTMDSetting>
-          <YTMDSetting v-model="discordPresenceEnabled" type="checkbox" name="Discord rich presence" @change="settingsChanged" />
-          <div v-if="discordPresenceEnabled && discordPresenceConnectionFailed" class="setting indented">
-            <p class="discord-failure">Discord connection could not be established after 30 attempts</p>
-            <button @click="restartDiscordPresence">Retry</button>
-          </div>
-          <YTMDSetting 
-          v-model="discordPresenceListening" 
-          type="checkbox" name="Discord presence 
-          listening" @change="settingsChanged"/>
-          <div 
-          v-if="discordPresenceListening" 
-            class="setting indented">
+          <div>
+            <YTMDSetting v-model="discordPresenceEnabled" type="checkbox" name="Discord rich presence" @change="settingsChanged" />
+            <div v-if="discordPresenceEnabled && discordPresenceConnectionFailed" class="setting indented">
+              <p class="discord-failure">Discord connection could not be established after 30 attempts</p>
+              <button @click="restartDiscordPresence">Retry</button>
+            </div>
+            <div v-if="discordPresenceEnabled">
+              <YTMDSetting v-model="discordPresenceListening" type="checkbox" name="Discord presence listening" @change="settingsChanged" />
+              <div v-if="discordPresenceListening" class="setting indented"></div>
+            </div>
           </div>
           <YTMDSetting
             v-model="lastFMEnabled"
