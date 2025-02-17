10c8bbe067ef76587ff3d3a43b8379c5daae47b6
brenc <bren.cooper.brown@gmail.com>
2025-02-17 17:19:07 -0500

    Fix showing the window when creating the view, was showing a weird "start playback" button in top left before.

--- src/main/index.ts
+++ src/main/index.ts
@@ -1030,6 +1030,17 @@ const createYTMView = (): void => {
       autoplayPolicy: store.get("playback.continueWhereYouLeftOffPaused") ? "document-user-activation-required" : "no-user-gesture-required"
     }
   });
+
+  if (mainWindow) {
+    mainWindow.addBrowserView(ytmView);
+    ytmView.setBounds({
+      x: 0,
+      y: 36,
+      width: mainWindow.getContentBounds().width,
+      height: mainWindow.getContentBounds().height - 36
+    });
+  }
+
   companionServer.provide(store, memoryStore, ytmView);
   customCss.provide(store, ytmView);
   ratioVolume.provide(ytmView);
