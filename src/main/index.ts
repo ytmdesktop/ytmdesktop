5b3654887d5d3ef264c1ee5da66b101ebce4776b
Jordan <jordantaylorlewis@gmail.com>
2025-08-05 21:51:23 -0400

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1634,7 +1634,7 @@ app.on("ready", async () => {
   ipcMain.on("ytmView:storeStateChanged", (event, queue, likeStatus, volume, muted, adPlaying, shuffled) => {
     if (event.sender !== ytmView.webContents) return;
 
-    playerStateStore.updateFromStore(queue, likeStatus, volume, muted, adPlaying);
+    playerStateStore.updateFromStore(queue, likeStatus, volume, muted, adPlaying, shuffled);
   });
 
   ipcMain.on("ytmView:switchFocus", (event, context) => {
